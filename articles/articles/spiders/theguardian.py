85a5d50feb9cb0c7427fa183b094557831ce37c4
ashrafnabilr <ashraf.nabil.r@gmail.com>
2021-03-14 17:46:00 +0200

    Parse required info and remove HTML tags using regular expressions lib (re)

--- articles/articles/spiders/theguardian.py
+++ articles/articles/spiders/theguardian.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import xml.etree.ElementTree as ET
+import re
 
 
 class TheguardianSpider(scrapy.Spider):
     name = 'theguardian'
     allowed_domains = ['theguardian.com']
     start_urls = ['https://www.theguardian.com/world/rss']
+
+    @staticmethod
+    def cleanHTML(raw_html):
+        reCompare = re.compile('<.*?>|Continue reading...')
+        cleanText = re.sub(reCompare, '', raw_html)
+        return cleanText
 
     def parse(self, response):
         string_xml = response.body
@@ -19,7 +26,7 @@ class TheguardianSpider(scrapy.Spider):
 
                 if child.tag == 'title' or child.tag == 'link' or \
                         child.tag == 'description' or child.tag == 'pubDate':
-                    news[child.tag] = child.text
+                    news[child.tag] = TheguardianSpider.cleanHTML(child.text)
                 # special checking for author tag
                 elif child.tag == '{http://purl.org/dc/elements/1.1/}creator':
                     news['creator'] = child.text
