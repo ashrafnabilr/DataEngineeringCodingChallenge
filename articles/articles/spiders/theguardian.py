ef06dcca6b72398d1fbfd7992064f291f372875a
ashrafnabilr <ashraf.nabil.r@gmail.com>
2021-03-14 18:47:55 +0200

    Store parsed data in Mongodb Atlas DB + add Mongodb username and password in the system environment variables (for security)

--- articles/articles/spiders/theguardian.py
+++ articles/articles/spiders/theguardian.py
@@ -2,12 +2,20 @@
 import scrapy
 import xml.etree.ElementTree as ET
 import re
+import os
+import pymongo
 
 
+# Please make sure before using this class that both 'mongodb_user' and
+# 'mongodb_password' are defined in the system environment variables
+# based on the user mongodb atlas user name and password
 class TheguardianSpider(scrapy.Spider):
     name = 'theguardian'
     allowed_domains = ['theguardian.com']
     start_urls = ['https://www.theguardian.com/world/rss']
+    CONNECTION_STRING = "mongodb+srv://" + os.environ.get('mongodb_user') + ":" + os.environ.get(
+        'mongodb_password') + "@cluster0.6sidv.mongodb.net/articles?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(CONNECTION_STRING)
 
     @staticmethod
     def cleanHTML(raw_html):
@@ -31,4 +39,8 @@ class TheguardianSpider(scrapy.Spider):
                 elif child.tag == '{http://purl.org/dc/elements/1.1/}creator':
                     news['creator'] = child.text
             newsItems.append(news)
-        print(newsItems)
+
+        db = self.client['articles']
+        col = db['data']
+        col.delete_many({})
+        col.insert_many(newsItems)
