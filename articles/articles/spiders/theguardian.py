642c414318f7153411217d67a257628cc31e5edd
ashrafnabilr <ashraf.nabil.r@gmail.com>
2021-03-14 17:42:03 +0200

    Parse response, extract the required info and add them to a dict

--- articles/articles/spiders/theguardian.py
+++ articles/articles/spiders/theguardian.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import xml.etree.ElementTree as ET
 
 
 class TheguardianSpider(scrapy.Spider):
@@ -8,4 +9,19 @@ class TheguardianSpider(scrapy.Spider):
     start_urls = ['https://www.theguardian.com/world/rss']
 
     def parse(self, response):
-        print(response.body)
+        string_xml = response.body
+        root = ET.fromstring(string_xml)
+        newsItems = []
+
+        for item in root.findall('./channel/item'):
+            news = {}
+            for child in item:
+
+                if child.tag == 'title' or child.tag == 'link' or \
+                        child.tag == 'description' or child.tag == 'pubDate':
+                    news[child.tag] = child.text
+                # special checking for author tag
+                elif child.tag == '{http://purl.org/dc/elements/1.1/}creator':
+                    news['creator'] = child.text
+            newsItems.append(news)
+        print(newsItems)
